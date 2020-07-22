43cd073b332802d75f4e912dfc0745082c8a9da9
Kamran Barlas <kamran_barlas@hotmail.com>
2020-07-22 17:02:02 +0500

    //

--- tinydeal/tinydeal/spiders/special_offers.py
+++ tinydeal/tinydeal/spiders/special_offers.py
@@ -14,5 +14,9 @@ class SpecialOffersSpider(scrapy.Spider):
                 'discoutned_price': product.xpath(".//div[@class='p_box_price']/span[1]/text()").get(),
                 'original_price': product.xpath(".//div[@class='p_box_price']/span[2]/text()").get()
             }
+        next_page = response.xpath("//a[@class='nextPage']/@href").get()
+
+        if next_page:
+            yield scrapy.Request(url=next_page,callback=self.parse)
 
 	
